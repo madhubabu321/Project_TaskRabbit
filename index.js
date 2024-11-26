8c7bc182d9a8b28bb2c1c307a490236a641853c3
madhubabu321 <buddagarimadhubabu321@gmail.com>
2024-11-26 15:01:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+
+
+function checkAllInputs(){
+
+function checkUserName(){
+let userName=document.getElementById("username");
+let userNameValue=userName.value;
+console.log(userNameValue);
+if (/^[a-z]+$/.test(userNameValue)) {
+    alert("Valid User Name ")
+}
+}
+
+
+function checkPass(){
+    let pass=document.getElementById("password");
+    let passValue=pass.value;
+    if(passValue.length<8){
+        let errMessage=document.createElement("p")
+        errMessage.innerHTML="password Length should be greater than length 8";
+        let passError=document.getElementById("pass-error");
+        passError.innerHTML="";
+        passError.append(errMessage);
+        
+    }
+}
+function checkGmail(){
+    let emailCheck=document.getElementById("email");
+    let emailCheckValue=emailCheck.value;
+    let emailError=document.getElementById("email-error");
+    let emailErrornotification=document.createElement("p");
+    emailErrornotification.innerhtml="Enter valid Email" ;
+    if(emailCheckValue.endsWith!="@gmail.com"){
+        emailError.append(emailErrornotification);
+    }
+}
+}
